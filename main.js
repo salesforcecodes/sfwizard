c7672a2a8f36e6ac3ce1a181712c751a84a62544
SFWizard <forhub@gmail.com>
2021-03-31 21:45:04 -0600

    Updating dynamic CORS whitelisting:

--- main.js
+++ main.js
@@ -4,20 +4,20 @@ import cors from 'cors';
 import api from './routes/index';
 
 var allowlist = ['http://localhost:3000/', 'https://salesforcecodes.github.io/']
-var corsOptionsDelegate = (req, callback) => {
-    var corsOptions;
-    if (allowlist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true }
-    } else {
-        corsOptions = { origin: false }
+var corsOptions = {
+    origin: (origin, callback) => {
+        if (allowlist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
     }
-    callback(null, corsOptions)
 }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/api', cors(corsOptionsDelegate), api);
+app.use('/api', cors(corsOptions), api);
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => {
