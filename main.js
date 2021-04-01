7e13f05eed6962e1025e0aa9b9bc6d45e9695c11
SFWizard <forhub@gmail.com>
2021-03-31 21:58:22 -0600

    Allowing CORS on all domains

--- main.js
+++ main.js
@@ -3,23 +3,24 @@ const app = express();
 import cors from 'cors';
 import api from './routes/index';
 
-var allowlist = ['http://localhost:3000/', 'https://salesforcecodes.github.io/']
-var corsOptions = {
-    origin: (origin, callback) => {
-        if (allowlist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
+// var allowlist = ['http://localhost:3000/', 'http://localhost:4000/', 'https://salesforcecodes.github.io/']
+// var corsOptionsDelegate = function (req, callback) {
+//     var corsOptions;
+//     if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//         corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//     } else {
+//         corsOptions = { origin: false } // disable CORS for this request
+//     }
+//     callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-app.use('/api', cors(corsOptions), api);
+app.use('/api', api);
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Application is running on ', process.env.PORT || 3000);
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Application is running on ', process.env.PORT || 4000);
 });
